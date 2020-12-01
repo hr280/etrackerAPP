0ee4efe00568e28a8edc0bf134474074ee891700
Hassan Raza <64700946+hr280@users.noreply.github.com>
2020-12-01 16:18:32 +0500

    E-Tracker

---
+++ src/context/GlobalState.js
@@ -0,0 +1,40 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+//initial-state
+const initialState = {
+  transactions: []
+}
+
+//create context APi
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
+    {children}
+  </GlobalContext.Provider>);
+}
+
+
